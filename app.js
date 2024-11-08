fed6de5b3eade2049571ca0abacf14dba9cc1f5c
Pablo Méndez <amandrangopablo@gmail.com>
2024-11-08 12:34:45 -0500

    style: :art: adding style to the application

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+let container= document.getElementById("buttonHello")
 let buttonHello= document.createElement('button')
 
 buttonHello.innerText= "Click me"
 
 buttonHello.addEventListener('click', () => alert("hello world with javascript :)"))
 
-document.body.append(buttonHello)
+container.appendChild(buttonHello)
