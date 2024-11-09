dbd58cc8bc14839e9d70219c3c0e411d1ae2dd18
Pablo Méndez <amandrangopablo@gmail.com>
2024-11-09 11:54:09 -0500

    build: :art: New structure of code and include dockerfile
    Docker file contain the dependencies for serve the static files in nginx

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-let container= document.getElementById("buttonHello")
 let buttonHello= document.createElement('button')
 
 buttonHello.innerText= "Click me"
 
-buttonHello.addEventListener('click', () => alert("hello world with javascript :)"))
+buttonHello.addEventListener('click', () => alert("hello world from javascript"))
 
-container.appendChild(buttonHello)
+document.body.append(buttonHello)
